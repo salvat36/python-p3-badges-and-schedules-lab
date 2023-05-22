b6a8fc1de634e77d4627110442961c6b36f2cf61
Ryan <salvat36@gmail.com>
2023-05-22 15:35:52 -0500

    passing labs

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,19 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return None
+    return [f"Hello, my name is {name}." for name in names]
 
 def assign_rooms(names):
-    return None
+    list = []
+    for i in range(len(names)):
+        list.append(f"Hello, {names[i]}! You'll be assigned to room {i+1}!")
+    return list
 
 def printer(names):
-    return None
+    badges = batch_badge_creator(names)
+    for badge in badges:
+        print(badge)
+    rooms = assign_rooms(names)
+    for room in rooms:
+        print(room)
